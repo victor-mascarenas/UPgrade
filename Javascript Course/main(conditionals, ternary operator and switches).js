5cf926dc75da93aa3588d352d77aa5420e39b628
Víctor J. G. Mascareñas <vijagama@outlook.es>
2021-12-28 10:38:43 -0600

    Conditionals
    Practice with conditionals, ternary operator and switches

---
+++ Javascript Course/main(conditionals, ternary operator and switches).js
@@ -0,0 +1,40 @@
+//Conditionals
+const x = '10';
+if (x == 10) {//matches values, not data type
+    console.log('X is 10');
+}
+if (x === 10) {//matches values and data type
+    console.log('X is 10');
+} else if (x > 10) {
+    console.log('X is greater than 10');
+} else {
+    console.log('X is less 10');
+}
+
+const y = 10;
+if (x > 5 || y > 10) {
+    console.log('x is more than 5 or y is more than 10')
+}
+
+if (x > 5 && y > 10) {
+    console.log('x is more than 5 and y is more than 10')
+}
+
+//Thernary operator
+const val = 10;
+const color = val > 10 ? 'red' : 'blue';
+console.log(`Color: ${color}`);
+
+//Switches
+console.log('Switch');
+switch(color) {
+    case 'red':
+        console.log('Color is red');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+        break;
+    default:
+        console.log('Color is not red or blue');
+        break;
+}
