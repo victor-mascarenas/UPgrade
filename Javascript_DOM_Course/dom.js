f2e3d7a73b2c6056b49b8006f3627d3405d9c64e
Víctor J. G. Mascareñas <vijagama@outlook.es>
2021-12-28 15:12:44 -0600

    Traversing the DOM
    Practice with querySelcetor, parentNode, parentElement, childNodes, children, firstChild, firstElementChild, lastChild, lastElementChild, nextSibling, nextElementSibling, previousSibling, previousElementSibling, createElement, createTextNode, appendChild and insertBefore

--- Javascript_DOM_Course/dom.js
+++ Javascript_DOM_Course/dom.js
@@ -66,7 +66,7 @@ var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.color = 'coral'; */
 
 //Query selector all
-var titles = document.querySelectorAll('.title');
+/* var titles = document.querySelectorAll('.title');
 console.log(titles);
 titles[0].textContent = 'hello';
 
@@ -77,4 +77,62 @@ for (let element of odd) {
 }
 for (let element of even) {
     element.style.background = '#ccc';
-}
+} */
+
+//Traversing the DOM
+//var itemsList = document.querySelector('#items');
+/* console.log(itemsList.parentNode);//Parent node of UL
+itemsList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemsList.parentNode.parentNode.parentNode); */
+
+/* console.log(itemsList.parentElement);//Parent node of UL
+itemsList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemsList.parentElement.parentElement.parentElement); */
+
+//Child nodes
+/* console.log(itemsList.childNodes);
+console.log(itemsList.children);
+console.log(itemsList.children[1]);
+itemsList.children[1].style.backgroundColor = 'yellow'; */
+
+//first child
+//console.log(itemsList.firstChild);
+
+//Element child
+//console.log(itemsList.firstElementChild);
+//itemsList.firstElementChild.textContent = 'Hello 1';
+
+//last child
+//console.log(itemsList.lastChild);
+
+//last element child
+//console.log(itemsList.lastElementChild);
+//itemsList.lastElementChild.textContent = 'Hello 1';
+
+//Next sibling
+//console.log(itemsList.nextSibling);
+
+//Next element sibling
+//console.log(itemsList.nextElementSibling);
+
+//Previous sibling
+//console.log(itemsList.previousSibling);
+
+//Previous element sibling
+//console.log(itemsList.previousElementSibling);
+//itemsList.previousElementSibling.style.color = 'green';
+
+//Create element
+var newDiv = document.createElement('div');
+newDiv.className = 'hello';
+newDiv.id = 'hello1';
+newDiv.setAttribute('title', 'Hello Div');
+
+//Create text node
+var newDivText = document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+newDiv.style.fontSize = '30px';
