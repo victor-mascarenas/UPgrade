650c68d172702442ad92624fa019e87141dc27d4
Víctor J. G. Mascareñas <vijagama@outlook.es>
2021-12-30 10:18:05 -0600

    Callbacks and promises
    Practice with callbacks and promises

---
+++ Javascript_Async_Course/callbacks.js
@@ -0,0 +1,36 @@
+const posts = [
+    {
+        title: "Post One",
+        body: "This is post one"
+    },
+    {
+        title: "Post Two",
+        body: "This is post two"
+    }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+getPosts();
+
+var post3 = {
+    title: "Post Three",
+    body: "This is post three"
+};
+
+createPost(post3, getPosts);
---
+++ Javascript_Async_Course/promises.js
@@ -0,0 +1,56 @@
+const posts = [
+    {
+        title: "Post One",
+        body: "This is post one"
+    },
+    {
+        title: "Post Two",
+        body: "This is post two"
+    }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+            if (!error) {
+                resolve();
+            } else {
+                reject('Error: Something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+var post3 = {
+    title: "Post Three",
+    body: "This is post three"
+};
+
+/* createPost(post3)
+    .then(getPosts)
+    .catch(error => console.log(error)); */
+
+    const promise1 = Promise.resolve('Hello World');
+    const promise2 = 10;
+    const promise3 = new Promise((resolve, reject) => {
+        setTimeout(resolve, 2000, 'Goodbye');
+    });
+    const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+        .then((res) => res.json());
+
+    Promise.all([promise1, promise2, promise3, promise4])
+        .then((values) => {
+            console.log(values);
+        });
