fb0127a07358858dfc2e87c763358af9aa9dc681
Víctor J. G. Mascareñas <vijagama@outlook.es>
2021-12-30 10:26:09 -0600

    Async function
    Practice with async functions and await

--- Javascript_Async_Course/promises.js
+++ Javascript_Async_Course/promises.js
@@ -42,15 +42,35 @@ var post3 = {
     .then(getPosts)
     .catch(error => console.log(error)); */
 
-    const promise1 = Promise.resolve('Hello World');
-    const promise2 = 10;
-    const promise3 = new Promise((resolve, reject) => {
-        setTimeout(resolve, 2000, 'Goodbye');
-    });
-    const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json());
 
-    Promise.all([promise1, promise2, promise3, promise4])
-        .then((values) => {
-            console.log(values);
-        });
+//Promise All
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Goodbye');
+});
+/* const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json()); */
+
+/*Promise.all([promise1, promise2, promise3, promise4])
+    .then((values) => {
+        console.log(values);
+    }); */
+
+//Async / await
+async function init() {
+    await createPost(post3);
+    getPosts();
+}
+
+//init();
+
+//Async / await with fetch
+
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+}
+
+fetchUsers();
