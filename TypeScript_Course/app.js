53f4ae13f6cd3b1ffb6c0ba30c169ba1f7808cd0
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-03 15:48:33 -0600

    Literal type
    Practice with iteral type

--- TypeScript_Course/app.js
+++ TypeScript_Course/app.js
@@ -1,16 +1,24 @@
 //Function which can combine numbers or strings
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion /*Only accepts this 2 values*/) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        //if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-nuber') {//Error, 'as-nuber' not allowed
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     //const result = input1 + input2;
+    /* if (resultConversion === 'as-number') {
+        result = +result;
+    } else {
+        result = result.toString();
+    } */
     return result;
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
+var combinedAgesS = combine('30', '26', 'as-number');
+console.log(combinedAgesS);
+var combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
