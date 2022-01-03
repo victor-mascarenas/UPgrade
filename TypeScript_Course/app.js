3ae38c57e9dbd946653f16070d5c3652ad315271
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-03 16:59:00 -0600

    Unknown type
    Practice with unknown type

--- TypeScript_Course/app.js
+++ TypeScript_Course/app.js
@@ -1,26 +1,5 @@
-function add(n1, n2) {
-    //function add(n1: number, n2: number): string {//Error
-    return n1 + n2;
-}
-function printResult(num) {
-    //function printResult(num: number): undefined {//not valid
-    console.log("Result: ".concat(num));
-}
-//With callback function
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-//let combineValues: Function;
-var combineValues; //Accepts any function which has 2 parameters (numbers) and returns a number
-combineValues = add; //Assigning function to variable
-//combineValues = printResult(5);//Error
-console.log(combineValues(8, 8)); //Using variable with 'add' type
-//let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-/* addAndHandle(10, 20, (result, b) => {//Error, expecting only one parameter
-    console.log(result);
-}); */ 
+var userInput;
+var userName;
+userInput = 'Max';
+userInput = 5;
+//userName = userInput;//Error (no error with 'any')
--- TypeScript_Course/app.ts
+++ TypeScript_Course/app.ts
@@ -1,34 +1,6 @@
-function add(n1: number, n2: number): number {
-//function add(n1: number, n2: number): string {//Error
-    return n1 + n2;
-}
+let userInput: unknown;
+let userName: string;
 
-function printResult(num: number) {//Void return type (:void)
-//function printResult(num: number): undefined {//not valid
-    console.log(`Result: ${num}`);
-}
-
-//With callback function
-function addAndHandle(n1:number, n2:number, cb:(num:number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(5, 12));
-
-//let combineValues: Function;
-let combineValues: (a:number, b:number) => number;//Accepts any function which has 2 parameters (numbers) and returns a number
-
-combineValues = add;//Assigning function to variable
-//combineValues = printResult(5);//Error
-
-console.log(combineValues(8, 8));//Using variable with 'add' type
-
-//let someValue: undefined;
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-});
-/* addAndHandle(10, 20, (result, b) => {//Error, expecting only one parameter
-    console.log(result);
-}); */
+userInput = 'Max';
+userInput = 5;
+//userName = userInput;//Error (no error with 'any')
