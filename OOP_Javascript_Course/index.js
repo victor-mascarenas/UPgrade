01089a3afc4d4e78bc543bfce65e1766063b8364
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-03 09:45:45 -0600

    Object literals
    Practice with object literal sintax

---
+++ OOP_Javascript_Course/index.js
@@ -0,0 +1,13 @@
+//Object literal sintax
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: () => {
+        console.log('draw');
+    }
+};
+
+circle.draw();
