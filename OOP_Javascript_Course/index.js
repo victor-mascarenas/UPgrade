33602a7a53293eff880c451c6744342d52f429c1
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-03 10:11:11 -0600

    Constructors
    Practice creating objects with constructors.
    
    - Functions are objects.

--- OOP_Javascript_Course/index.js
+++ OOP_Javascript_Course/index.js
@@ -8,4 +8,28 @@ function createCircle(radius) {
     };
 }
 
-createCircle(3).draw();
+//const circle = createCircle(3);
+
+//Constructor function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = () => {
+        console.log('draw');
+    }
+}
+
+console.log(Circle.name);
+console.log(Circle.lenght);
+console.log(Circle.constructor);
+
+const Circle1 = new Function('radius', `this.radius = radius;
+this.draw = () => {
+    console.log('draw');
+}`);
+
+const circle = new Circle1(1);
+
+console.log(circle);
+
+const circle2 = new Circle(1);
