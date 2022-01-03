4ab5e7e37aeea4793789e77e657f1f95a13bb9e3
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-03 15:17:54 -0600

    Enum
    Practice with 'enum' type

--- TypeScript_Course/app.js
+++ TypeScript_Course/app.js
@@ -1,10 +1,23 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR"; //2
+})(Role || (Role = {}));
+;
 //const person:object = {
+/* const person: {
+    name:string;
+    age:number;
+    hobbies:string[];
+    role:[number, string];//tupla
+} = { */ //same as object
 var person = {
-    //const person = {
     name: 'Maximilium',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    //role: [2, 'author']
+    role: Role.AUTHOR
 };
 //person.role.push('admin');//TS doesnt check this
 //person.role[1] = 10;
@@ -19,4 +32,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) { //TypeScript knows
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     //console.log(hobby.map());//Error
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
--- TypeScript_Course/app.ts
+++ TypeScript_Course/app.ts
@@ -1,15 +1,23 @@
+enum Role {
+    ADMIN,//0
+    //ADMIN = 5,//Setting value
+    READ_ONLY,//1
+    AUTHOR//2
+};
+
 //const person:object = {
-const person: {
+/* const person: {
     name:string;
     age:number;
     hobbies:string[];
     role:[number, string];//tupla
-} = { //same as object
-//const person = {
+} = { */ //same as object
+const person = {
     name: 'Maximilium',
     age: 30,
     hobbies: ['Sports', 'Cooking'],//String array
-    role: [2, 'author']
+    //role: [2, 'author']
+    role: Role.AUTHOR
 }
 
 //person.role.push('admin');//TS doesnt check this
@@ -27,4 +35,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {//TypeScript knows 'hobby' is gonna be a string, because of the types
     console.log(hobby.toUpperCase());
     //console.log(hobby.map());//Error
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
