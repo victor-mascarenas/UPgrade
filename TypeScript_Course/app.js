4e8733cb3018f002e07fa162ca2e8e08afcb0d44
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-04 09:23:27 -0600

    Never type
    Practice with the never type

--- TypeScript_Course/app.js
+++ TypeScript_Course/app.js
@@ -3,3 +3,7 @@ var userName;
 userInput = 'Max';
 userInput = 5;
 //userName = userInput;//Error (no error with 'any')
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured', 500);
--- TypeScript_Course/app.ts
+++ TypeScript_Course/app.ts
@@ -4,3 +4,9 @@ let userName: string;
 userInput = 'Max';
 userInput = 5;
 //userName = userInput;//Error (no error with 'any')
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+generateError('An error occured', 500);
