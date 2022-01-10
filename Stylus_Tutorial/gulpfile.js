6b5b81a59c53d9db6b54c662cdfb457333b87448
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-10 15:42:04 -0600

    Packages with Gulp
    Practice using other packages with Stylus & Gulp

--- Stylus_Tutorial/gulpfile.js
+++ Stylus_Tutorial/gulpfile.js
@@ -1,9 +1,13 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+var typo = require('typographic');
+var nib = require('nib')
 
 gulp.task('styles', () => {
     return gulp.src('styles/style.styl')
-        .pipe(stylus())
+        .pipe(stylus({
+            use: [typo(), nib()]
+        }))
         .pipe(gulp.dest('./styles'));
 });
 gulp.task('watch-styles', () => {
