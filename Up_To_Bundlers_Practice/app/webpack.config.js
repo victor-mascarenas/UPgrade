12b79249c75ee34e10fd039dc4e49b10dc7dbe40
Víctor J. G. Mascareñas <vijagama@outlook.es>
2022-01-14 15:17:56 -0600

    Project structure
    Added project structure, dependencies and configuration for JS app with Webpack

---
+++ Up_To_Bundlers_Practice/app/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].scss',
+            chunkFilename: '[id].css'
+        })
+    ]
+}
