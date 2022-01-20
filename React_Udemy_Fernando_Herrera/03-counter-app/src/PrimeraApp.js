91f0e4856eb28327309c13571ea8443f9a44c731
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 10:04:37 -0600

    Fragments
    Practice with fragments

--- React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React/* , { Fragment } */ from "react";
 
 const PrimeraApp = () => {
-    return <h1>Hola mundo</h1>;
+    /* return (
+        <Fragment>
+            <h1>Hola mundo</h1>
+            <p>Mi primera aplicacion</p>
+        </Fragment>
+    ); */
+    /* return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Mi primera aplicacion</p>
+        </>
+    ); */
+    return (
+        <div>
+            <h1>Hola mundo</h1>
+            <p>Mi primera aplicacion</p>
+        </div>
+    );
 }
 
 export default PrimeraApp;
