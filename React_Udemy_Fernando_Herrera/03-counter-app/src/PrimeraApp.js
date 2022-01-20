bb81d3253dfcf37015869784fbbce3e5dc811e31
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 10:13:40 -0600

    Printing variables
    Practice printing primitives, arrays and objects

--- React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
@@ -1,21 +1,20 @@
-import React/* , { Fragment } */ from "react";
+import React from "react";
 
 const PrimeraApp = () => {
-    /* return (
-        <Fragment>
-            <h1>Hola mundo</h1>
-            <p>Mi primera aplicacion</p>
-        </Fragment>
-    ); */
-    /* return (
-        <>
-            <h1>Hola mundo</h1>
-            <p>Mi primera aplicacion</p>
-        </>
-    ); */
+    /* const saludo = 'Hola mundo const';
+    const saludo = 123;
+    const saludo = 23.345;
+    const saludo = [1, 'asfsd']; */
+    /* const saludo = {
+        nombre: 'Victor',
+        edad: 24
+    }; */
+    const saludo = 'Hola mundo';
     return (
         <div>
-            <h1>Hola mundo</h1>
+            <h1>{saludo}</h1>
+            {/* <h1>{JSON.stringify(saludo)}</h1> */}
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
             <p>Mi primera aplicacion</p>
         </div>
     );
