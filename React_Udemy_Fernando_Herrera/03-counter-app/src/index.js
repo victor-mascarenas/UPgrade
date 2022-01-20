8bfe8a5de96b6425e818fd56e4f4e7681e340e8f
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 09:57:00 -0600

    First component
    Added first component and global style

--- React_Udemy_Fernando_Herrera/03-counter-app/src/index.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css';
 
-const saludo = <h1>Hola mundo</h1>;
 const divRoot = document.querySelector('#root');
-ReactDom.render(saludo, divRoot);
+ReactDom.render(<PrimeraApp/>, divRoot);
