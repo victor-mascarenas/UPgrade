ec1a096e1b6c0faef1ad6655163c60c5e5261002
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 17:04:54 -0600

    Expect
    Practice with 'expect' & 'toBe' test methods & 'describe'

--- React_Udemy_Fernando_Herrera/03-counter-app/src/tests/demo.test.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/tests/demo.test.js
@@ -1,6 +1,10 @@
-test('debe ser true', () => {
-    const isActive = true;
-    if (isActive) {
-        throw new Error('No esta activo');
-    }
+describe('Pruebas en el archivo de.test.js', () => {
+    test('deben de ser iguales los string', () => {
+        //1. Inicializacion (Arrange)
+        const mensaje = 'Hola mundo';
+        //2. Estimulo (Act)
+        const mensaje2 = 'Hola mundo ';
+        //3. Observar (Assert)
+        expect(mensaje).toBe(mensaje2);
+    });
 });
