a4f61a60147ca258322b23161001a110d396ec21
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 11:07:47 -0600

    Events
    Practice with events

--- React_Udemy_Fernando_Herrera/03-counter-app/src/CounterApp.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/CounterApp.js
@@ -2,10 +2,14 @@ import React from "react";
 import { PropTypes } from "prop-types";
 
 const CounterApp = ({value}) => {
+    const buttonOnClick = (e) => {
+        console.log(e.target);
+    };
     return (
         <div>
             <h1>Counter App</h1>
             <h2>{value}</h2>
+            <button onClick={buttonOnClick}>+1</button>
         </div>
     );
 }
