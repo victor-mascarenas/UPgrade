d201d089f0714d614d525d42cf9102879ce66bb7
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-20 10:41:15 -0600

    Defaults
    Practice defining default property values

--- React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/PrimeraApp.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 
-const PrimeraApp = ({saludo}) => {
-    /* if (!saludo) {
-        throw new Error('El saludo es necesario');
-    } */
+//const PrimeraApp = ({saludo, subtitulo = 'Soy un subtitulo'}) => {
+const PrimeraApp = ({saludo, subtitulo}) => {
     return (
         <div>
             <h1>{saludo}</h1>
-            <p>Mi primera aplicacion</p>
+            <p>{subtitulo}</p>
         </div>
     );
 }
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
+}
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
 export default PrimeraApp;
