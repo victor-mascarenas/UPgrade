1a5b691a555854d4d043712d913ab9969669e021
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-21 16:45:54 -0600

    Enzyme introduction
    Introduction to component testing with Enzyme, Enzyme to Json & snapshots

--- React_Udemy_Fernando_Herrera/03-counter-app/src/tests/PrimeraApp.test.js
+++ React_Udemy_Fernando_Herrera/03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,13 +1,18 @@
 import React, { render } from "@testing-library/react";
+import {shallow} from 'enzyme';
 import PrimeraApp from "../PrimeraApp";
 
 describe('Pruebas ne <PrimeraApp/>', () => {
-    test('Debe de mostrar el mensaje "Hola soy Victor"', () => {
+    /* test('Debe de mostrar el mensaje "Hola soy Victor"', () => {
         const saludo = 'Hola osy Victor';
-        /* const wrapper = render(<PrimeraApp saludo={saludo}/>);
-        wrapper.getByText(); */
+        //const wrapper = render(<PrimeraApp saludo={saludo}/>);
+        //wrapper.getByText();
         const {getByText} = render(<PrimeraApp saludo={saludo}/>);
         expect(getByText(saludo)).toBeInTheDocument();
+    }); */
+    test('Debe de mostrar <PrimeraApp/> correctamente', () => {
+        const saludo = 'Hola soy Victor';
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>);
+        expect(wrapper).toMatchSnapshot();
     });
-    
 });
