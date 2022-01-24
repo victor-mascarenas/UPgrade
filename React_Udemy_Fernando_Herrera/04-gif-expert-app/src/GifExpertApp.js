bfe28342d4bf4dfdd38e3810d10a19d74a9f4a1d
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-24 09:59:17 -0600

    Categories
    Added categories & new category button

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/GifExpertApp.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 const GifExpertApp = () => {
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball', 'Naruto']
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball', 'Naruto']);
+    
+    const addButtonOnClick = () => {
+        const newItem = 'Family Guy';
+        setCategories([...categories, newItem]);
+    };
+
     return(
         <>
             <h2>GifExpertApp</h2>
             <hr/>
+            <button onClick={addButtonOnClick}>Agregar</button>
+            <ul>
+                {
+                    categories.map((category, key) => {
+                        return <li key={`${category}_${key}`}>{category}</li>;
+                    })
+                }
+            </ul>
         </>
     );
 }
