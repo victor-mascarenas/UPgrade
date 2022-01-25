957ce9476629d9b78d1e77aa8e08662da1f1af2d
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-25 09:17:44 -0600

    UseEffect
    Practice with useEffect hook

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/components/GifGrid.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const GifGrid = ({category}) => {
+    const [count, setCount] = useState(0);
+
+    //Se ejecuta cuando el componente es renderizado por primera vez
+    useEffect(() => {
+        getGifs();
+    }, []);
+
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=naruto&limit=10&api_key=RHHTxfPP13Hp2qPjeceyWuqAzfCetKkv';
         const res = await fetch(url);
@@ -15,11 +22,12 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     };
 
-    getGifs();
+    //getGifs();
 
     return(
         <>
             <h3>{category}</h3>
+            <button onClick={() => setCount(count + 1)}></button>
         </>
     );
 };
