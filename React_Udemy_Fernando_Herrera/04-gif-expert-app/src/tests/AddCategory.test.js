eced4e18637f5a214f181dcf86e8dd515ea08b6c
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-26 10:47:34 -0600

    Input events
    Practice with testing simulation input onChange event

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/AddCategory.test.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/AddCategory.test.js
@@ -3,10 +3,22 @@ import {shallow} from 'enzyme';
 import AddCategory from '../components/AddCategory';
 
 describe('Pruebas en <AddCategory/>', () => {
+    const setCategories = () => {};
+    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+
     test('Debe de mostrarse correctamete', () => {
-        const setCategories = () => {};
-        const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
         expect(wrapper).toMatchSnapshot();
+    });
+    test('Debe de cambiar la caja de teto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola mundo';
+        const e = {
+            target: {
+                value: value
+            }
+        };
+        input.simulate('change', e);//Enviar 'e';
+        //expect(input.state('inputValue')).toBe(value);
     });
     
 });
