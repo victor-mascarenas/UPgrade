69799f1f79358e7dfc76075a3a4adab24a96a7f9
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-26 09:51:58 -0600

    GrifGridItem tests
    Tests done for GifGridItem component

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/GifGridItem.test.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/GifGridItem.test.js
@@ -4,9 +4,24 @@ import GifGridItem from '../components/GifGridItem';
 
 describe('Pruebas en GifGridItem', () => {
     const title = 'Un titulo';
-    const url = 'https://localhost:8080/img/1.gif'
+    const url = 'https://localhost:8080/img/1.gif';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+    
     test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
     });
+    test('Debe de tener un parrafo con el titulo', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    });
+    test('Debe de tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+    test('Debe tener la clase de animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        expect(div.prop('className')).toContain('animate__fadeIn');
+    });
+    
 });
