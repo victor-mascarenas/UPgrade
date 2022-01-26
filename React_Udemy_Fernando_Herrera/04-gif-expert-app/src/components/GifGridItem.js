2f987752227cbb1b9a24a3496137995a47aba931
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-26 09:40:44 -0600

    Implemented PropTypes
    PropTypes implemented in GiGridItem component

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/components/GifGridItem.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/components/GifGridItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
-const GifGridItem = ({id, title, url}) => {
+const GifGridItem = ({title, url}) => {
     return (
         <div className="card animate__animated animate__fadeIn">  
             <img src={url} alt={title}/>
@@ -8,5 +9,9 @@ const GifGridItem = ({id, title, url}) => {
         </div>
     );
 };
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+}
 
 export default GifGridItem;
--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/GifGridItem.test.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/GifGridItem.test.js
@@ -3,8 +3,10 @@ import {shallow} from 'enzyme';
 import GifGridItem from '../components/GifGridItem';
 
 describe('Pruebas en GifGridItem', () => {
+    const title = 'Un titulo';
+    const url = 'https://localhost:8080/img/1.gif'
     test('Debe mostrar el componente correctamente', () => {
-        const wrapper = shallow(<GifGridItem/>);
+        const wrapper = shallow(<GifGridItem title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
     });
 });
