c9ca50d388d037f53ff75db0f785dcda4f285d2a
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-26 17:17:50 -0600

    Submit event
    Practice with testing simulation input events

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/AddCategory.test.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/AddCategory.test.js
@@ -3,8 +3,13 @@ import {shallow} from 'enzyme';
 import AddCategory from '../components/AddCategory';
 
 describe('Pruebas en <AddCategory/>', () => {
-    const setCategories = () => {};
-    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    });
 
     test('Debe de mostrarse correctamete', () => {
         expect(wrapper).toMatchSnapshot();
@@ -20,5 +25,19 @@ describe('Pruebas en <AddCategory/>', () => {
         input.simulate('change', e);//Enviar 'e';
         //expect(input.state('inputValue')).toBe(value);
     });
-    
+    test('No debe de postear la informacion con submit', () => {
+        wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+    test('Debe de postear la informacion con submit', () => {
+        const input = wrapper.find('input');
+        const e = {
+            target: {
+                value: 'Lego'
+            }
+        };
+        input.simulate('change', e);
+        wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+        expect(setCategories).toHaveBeenCalledTimes(1);
+    });
 });
