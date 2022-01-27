e405f5f0904d0578b069e2ead2d4083058b8758b
victor-mascarenas <victor-javier.garcia@atos.net>
2022-01-27 10:45:57 -0600

    Update useFetchGifs.test.js
    Practice with testing hook waiting for state change

--- React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ React_Udemy_Fernando_Herrera/04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -2,12 +2,20 @@ import useFetchGifs from '../../hooks/useFetchGifs';
 import {renderHook} from '@testing-library/react-hooks';
 
 describe('Pruebas en useFetchGifs', () => {
-    test('Debe de retornar el estado inicial', () => {
+    test('Debe de retornar el estado inicial', async () => {
         const category = 'Hinata';
-        const {result} = renderHook(() => useFetchGifs(category));
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs(category));
         const {data, loading} = result.current;
+        await waitForNextUpdate();
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    });
+    test('Debe de retornar un arreglo de imagenes y el loading en false', async () => {
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('Godzilla'));
+        await waitForNextUpdate();
+        const {data, loading} = result.current;
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     });
     
 });
