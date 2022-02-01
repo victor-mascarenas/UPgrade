9eaa22c90be25d10ab5c50002abd3804cd701b27
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-01 16:51:33 -0600

    Context
    Practice creating context

--- React_Udemy_Fernando_Herrera/05-hook-app/src/Components/09-useContext/HomePage.js
+++ React_Udemy_Fernando_Herrera/05-hook-app/src/Components/09-useContext/HomePage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomePage = () => {
+    const context = useContext(UserContext);
+
     return <div>
         <h1>Home page</h1>
         <hr/>
--- React_Udemy_Fernando_Herrera/05-hook-app/src/Components/09-useContext/MainApp.js
+++ React_Udemy_Fernando_Herrera/05-hook-app/src/Components/09-useContext/MainApp.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
-    /* return <div>
-        <h1>Main App</h1>
-        <hr/>
-        <AppRouter/>
-    </div>; */
-    return <AppRouter/>;
+    const user = {
+        id: 1234,
+        name: 'Victor',
+        email: 'vijagama@outlook.es'
+    };
+
+    return (
+        <UserContext.Provider value={user}>
+            <AppRouter/>
+        </UserContext.Provider>
+    );
 };
