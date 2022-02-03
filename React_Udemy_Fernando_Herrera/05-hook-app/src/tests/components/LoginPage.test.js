1684952718feedab4922e85c78b0152c26d0baca
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-03 17:00:01 -0600

    LoginPage tests
    Login page tests done

---
+++ React_Udemy_Fernando_Herrera/05-hook-app/src/tests/components/LoginPage.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import { UserContext } from '../../Components/09-useContext/UserContext';
+import { LoginPage } from '../../Components/09-useContext/LoginPage';
+import { act } from '@testing-library/react';
+
+describe('Pruebas en LoginPage', () => {
+    const user = {
+        id: 1234,
+        name: 'Victor',
+        email: 'vijagama@outlook.es'
+    };
+    const setUser = jest.fn((user) => {});
+    const wrapper = mount(
+        <UserContext.Provider value={{
+            user,
+            setUser
+        }}>
+            <LoginPage/>
+        </UserContext.Provider>
+    );
+
+    test('Debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+    test('Debe ejecutar el setUser con el argumento esperado', () => {
+        const btn = wrapper.find('button');
+        btn.simulate('click');
+        expect(setUser).toHaveBeenCalledWith(user);
+    });
+    
+});
