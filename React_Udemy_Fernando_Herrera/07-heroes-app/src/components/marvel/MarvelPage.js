caa3662c667bd1c54c8f61d1224ecf895d989168
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-04 09:41:00 -0600

    AppRouter
    AppRouter done

---
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/marvel/MarvelPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MarvelPage = () => {
+    return (
+        <div>
+            <h1>MARVEL Page</h1>
+        </div>
+    );
+};
---
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/search/SearchPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SearchPage = () => {
+    return (
+        <div>
+            <h1>Search Page</h1>
+        </div>
+    );
+};
