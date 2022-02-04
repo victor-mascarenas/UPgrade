41c956e1e201b817c7f34d46ce6c5f9da2e33403
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-04 11:18:17 -0600

    HeroCard
    HeroCard component done

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/components/hero/HeroList.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/hero/HeroList.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher = 'DC Comics'}) => {
     const heroes = getHeroesByPublisher(publisher);
     
     return (
-        <>
-            <h1>Hero List</h1>
-            <ul>
-                {
-                    heroes.map(hero => (
-                        <li key={hero.id}>
-                            {hero.superhero}
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
+            {
+                heroes.map(hero => (
+                    <HeroCard key={hero.id} hero={hero}/>
+                ))
+            }
+        </div>
     );
 };
