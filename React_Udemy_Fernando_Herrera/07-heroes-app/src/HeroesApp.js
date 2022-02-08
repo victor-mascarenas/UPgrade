240de04b5ebe0799f86dc75acc4517073980dd6f
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 11:00:50 -0600

    Auth contezt
    Applied context for authentication

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/HeroesApp.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/HeroesApp.js
@@ -1,7 +1,23 @@
+import { useReducer } from "react";
+import { AuthContext } from "./auth/authContext";
+import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
 
+const init = () => {
+    return JSON.parse(localStorage.getItem('user')) || {
+        logged: false
+    };
+}
+
 export const HeroesApp = () => {
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
     return (
-        <AppRouter/>
+        <AuthContext.Provider value={{
+            user,
+            dispatch
+        }}>
+            <AppRouter/>
+        </AuthContext.Provider>
     );
 };
