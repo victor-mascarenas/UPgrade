ab4d01a536b966fc275d548a980225d509a5719b
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 09:34:25 -0600

    Search page
    Search page design done

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/components/search/SearchPage.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/search/SearchPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import useForm from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchPage = () => {
     const [{searchText}, inputOnChange] = useForm({
         searchText: ''
     });
+
+    const heroesFiltered = getHeroesByName('algo');
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -23,6 +27,15 @@ export const SearchPage = () => {
                         <input type='text' placeholder='Buscar un heroe' className="form-control" name='searchText' autoComplete='off' value={searchText} onChange={inputOnChange}/>
                         <button type='submit' className='btn btn-outline-primary mt-3 col-12'>Buscar</button>
                     </form>
+                </div>
+                <div className='col-7'>
+                    <h4>Resultados</h4>
+                    <hr/>
+                    {
+                        heroesFiltered.map(hero => {
+                            return <HeroCard key={hero.id} hero={hero}/>;
+                        })
+                    }
                 </div>
             </div>
         </>
