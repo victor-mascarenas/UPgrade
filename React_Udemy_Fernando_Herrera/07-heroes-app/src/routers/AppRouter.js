f1869ecff0d1dc2dd0771223ee2d8ebc85b4d0e8
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 11:41:32 -0600

    Private routes
    Private routes added & authentication validation

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/routers/AppRouter.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/routers/AppRouter.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../components/login/LoginPage';
-
 import { DashboardRouter } from './DashboardRouter';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/login' element={<LoginPage/>}/>
-                <Route path='/*' element={<DashboardRouter/>}/>
+                <Route path='/*' element={
+                    <PrivateRoute>
+                        <DashboardRouter/>
+                    </PrivateRoute>
+                }/>
+                {/* <Route path='/*' element={<DashboardRouter/>}/> */}
             </Routes>
         </BrowserRouter>
     );
