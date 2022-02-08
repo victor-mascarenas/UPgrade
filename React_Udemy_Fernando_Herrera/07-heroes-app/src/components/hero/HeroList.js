6e007ce2b33a578f7446d42d3853c54371bc7290
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 08:56:57 -0600

    useMemo
    Memorizing hero list with useMemo hook

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/components/hero/HeroList.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/hero/HeroList.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher = 'DC Comics'}) => {
-    const heroes = getHeroesByPublisher(publisher);
+    let heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
     
     return (
         <div className='row rows-cols-1 row-cols-md-3 g-3'>
-            {
+            {   
                 heroes.map(hero => (
                     <HeroCard key={hero.id} hero={hero}/>
                 ))
