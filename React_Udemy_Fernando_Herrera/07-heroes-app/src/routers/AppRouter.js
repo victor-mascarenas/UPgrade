ba965d2a38b177eee5c187ed7d6a9c1b4f9c3632
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 11:54:46 -0600

    Public routes
    Public route done & user authentication validation

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/routers/AppRouter.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/routers/AppRouter.js
@@ -3,12 +3,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../components/login/LoginPage';
 import { DashboardRouter } from './DashboardRouter';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/login' element={<LoginPage/>}/>
+                <Route path='/login' element={
+                    <PublicRoute>
+                        <LoginPage/>
+                    </PublicRoute>
+                }/>
+                {/* <Route path='/login' element={<LoginPage/>}/> */}
                 <Route path='/*' element={
                     <PrivateRoute>
                         <DashboardRouter/>
