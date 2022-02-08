dbc339c06e55999cf506bc48e86ddf77ba28d147
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-08 11:17:12 -0600

    Login action
    Login action done

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/HeroesApp.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/HeroesApp.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { AuthContext } from "./auth/authContext";
 import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
@@ -11,6 +11,9 @@ const init = () => {
 
 export const HeroesApp = () => {
     const [user, dispatch] = useReducer(authReducer, {}, init);
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user));
+    }, [user]);
 
     return (
         <AuthContext.Provider value={{
--- React_Udemy_Fernando_Herrera/07-heroes-app/src/components/login/LoginPage.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/components/login/LoginPage.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
 
 export const LoginPage = () => {
+    const {dispatch} = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleLogin = () => {
+        const action= {
+            type: types.login,
+            payload: {
+                name: 'Victor',
+                logged: true
+            }
+        };
+        dispatch(action);
         navigate('/', {
             replace: true//Reemplazar el historico de navegacion del navegador
         });
