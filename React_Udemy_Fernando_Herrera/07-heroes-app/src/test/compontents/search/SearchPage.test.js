8dd74d0e0433ee972404d8d64b7d23ce067caed5
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-09 14:59:53 -0600

    SearchPage search tests
    Tests done for searching hero by query parameters

--- React_Udemy_Fernando_Herrera/07-heroes-app/src/test/compontents/search/SearchPage.test.js
+++ React_Udemy_Fernando_Herrera/07-heroes-app/src/test/compontents/search/SearchPage.test.js
@@ -19,4 +19,27 @@ describe('Pruebas en SearchPage', () => {
         );
         expect(wrapper.find('.card-text').text().trim()).toBe('Ingresa un termino de busqueda');
     });
+    test('Debe de mostrar a Batman y el input con el valor del queryString', () => {
+        const keyword = 'Batman';
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[`/search?q=${keyword}`]}>
+                <SearchPage/>
+            </MemoryRouter>
+        );
+        expect(wrapper.find('input').prop('value')).toBe(keyword);
+        const heroCard = wrapper.find('HeroCard');
+        expect(heroCard.length).toBe(1);
+        expect(heroCard.find('h5').text().trim()).toBe(keyword);
+    });
+    test('Debe de mostrar un error si no e encuentra el heroe', () => {
+        const keyword = 'batman123';
+        const wrapper = mount(
+            <MemoryRouter initialEntries={[`/search?q=${keyword}`]}>
+                <SearchPage/>
+            </MemoryRouter>
+        );
+        const errorCard = wrapper.find('ErrorCard');
+        expect(errorCard.exists()).toBeTruthy();
+        expect(errorCard.find('p').text().trim()).toBe(`Sin resultados para ${keyword}`);
+    });
 });
