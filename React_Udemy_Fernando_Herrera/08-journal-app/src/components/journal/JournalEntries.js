cdb32998de4d3bd3dcd71b3cad973b77fcceb45b
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-10 15:59:49 -0600

    Nothing component
    Added component for JournalScreen fro when no entry is selected

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/journal/JournalEntries.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/journal/JournalEntries.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { JournalEntry } from './JournalEntry';
 
 export const JournalEntries = () => {
-    const entries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const entries = [1, 2, 3, 4];
 
     return (
         <div className='jurnal__entries'>
