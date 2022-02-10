6bf8509b937016c240870eae0845101c9e7dd701
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-10 16:32:30 -0600

    Desing node
    Journal App design done

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/notes/NoteSection.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/notes/NoteSection.js
@@ -5,6 +5,13 @@ export const NoteSection = () => {
     return (
         <div className='notes__main-content'>
             <NoteAppBar/>
+            <div className='notes__content'>
+                <input type='text' placeholder='Some awesome title' className='notes__title-input'/>
+                <textarea placeholder='What happened today?' className='notes__text-area'></textarea>
+                <div className='notes__images'>
+                    <img src='https://bombaykitchencabinets.com/wp-content/uploads/2021/11/Modular-Kitchen-in-Etobicoke.png' alt='Cocina'/>
+                </div>
+            </div>
         </div>
     )
 }
