4e42fbd41e5a790a21d818f1430bac178081304a
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-10 14:28:10 -0600

    RegisterScreen
    RegisterScreen HTML & styling done

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/LoginScreen.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/LoginScreen.js
@@ -20,7 +20,7 @@ export const LoginScreen = () => {
                         </p>
                     </div>
                 </div>
-                <Link to='/auth/register'>Create new account</Link>
+                <Link className='link' to='/auth/register'>Create an account instead</Link>
             </form>
         </>
     )
--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegiterScreen = () => {
     return (
-        <div>
-            <h1>Register Screen</h1>
-        </div>
+        <>
+            <h3 className='auth__title'>Create an account</h3>
+            <form>
+                <input className='auth__input' type='text' placeholder='Name' name='name' autoComplete='off'/>
+                <input className='auth__input' type='text' placeholder='Email' name='email' autoComplete='off'/>
+                <input className='auth__input' type='password' placeholder='Password' name='password'/>
+                <input className='auth__input' type='password' placeholder='Repeat password' name='confirm-password'/>
+                <button className='btn btn-primary btn-block mb-5' type='submit'>Sign up</button>
+                <Link className='link' to='/auth/login'>Login instead</Link>
+            </form>
+        </>
     )
 }
