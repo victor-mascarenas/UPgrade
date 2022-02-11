16777b35c3a00bd6d7cd46c92b4bd58aff968efb
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-11 12:18:13 -0600

    RegisterScreen functionality
    Added useForm hook & submit event for registering users

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useForm from '../../hooks/useForm';
 
 export const RegiterScreen = () => {
+    const {values: formValues, inputOnChange} = useForm({
+        name: 'Victor',
+        email: 'nando@gmail.com',
+        password: '123456',
+        confirmPassword: '123456'
+    });
+    const {name, email, password, confirmPassword} = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        
+    };
+
+    const isFormValid = () => {
+
+    };
+
     return (
         <>
             <h3 className='auth__title'>Create an account</h3>
-            <form>
-                <input className='auth__input' type='text' placeholder='Name' name='name' autoComplete='off'/>
-                <input className='auth__input' type='text' placeholder='Email' name='email' autoComplete='off'/>
-                <input className='auth__input' type='password' placeholder='Password' name='password'/>
-                <input className='auth__input' type='password' placeholder='Repeat password' name='confirm-password'/>
+            <form onSubmit={handleRegister}>
+                <input className='auth__input' type='text' placeholder='Name' name='name' autoComplete='off' value={name} onChange={inputOnChange}/>
+                <input className='auth__input' type='text' placeholder='Email' name='email' autoComplete='off' value={email} onChange={inputOnChange}/>
+                <input className='auth__input' type='password' placeholder='Password' name='password' value={password} onChange={inputOnChange}/>
+                <input className='auth__input' type='password' placeholder='Repeat password' name='confirmPassword' value={confirmPassword} onChange={inputOnChange}/>
                 <button className='btn btn-primary btn-block mb-5' type='submit'>Sign up</button>
                 <Link className='link' to='/auth/login'>Login instead</Link>
             </form>
