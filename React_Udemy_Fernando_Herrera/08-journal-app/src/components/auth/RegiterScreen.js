c0a0b667324229cc934b228589be332836f0ce28
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-11 12:33:18 -0600

    Form validation
    Form validation practice with validator pkg

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/RegiterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import useForm from '../../hooks/useForm';
+import validator from 'validator';
 
 export const RegiterScreen = () => {
     const {values: formValues, inputOnChange} = useForm({
@@ -13,17 +14,36 @@ export const RegiterScreen = () => {
 
     const handleRegister = (e) => {
         e.preventDefault();
-        
+        if (isFormValid()) {
+            console.log('correcto');
+        }
     };
 
     const isFormValid = () => {
-
+        let valid = false;
+        if (name.trim().length === 0) {
+            console.log('Name is required');
+        } else if (!validator.isEmail(email)) {
+            console.log('Email is not valid');
+        } else if (email.trim().length === 0) {
+            console.log('Email is required');
+        } else if (password !== confirmPassword) {
+            console.log('Passwords are ot the same');
+        } else if (password.trim().length < 5) {
+            console.log('Password needs to be at least 5 characters long');
+        } else {
+            valid = true;
+        }
+        return valid;
     };
 
     return (
         <>
             <h3 className='auth__title'>Create an account</h3>
             <form onSubmit={handleRegister}>
+                <div className='auth__alert-error'>
+                    Campos no validos
+                </div>
                 <input className='auth__input' type='text' placeholder='Name' name='name' autoComplete='off' value={name} onChange={inputOnChange}/>
                 <input className='auth__input' type='text' placeholder='Email' name='email' autoComplete='off' value={email} onChange={inputOnChange}/>
                 <input className='auth__input' type='password' placeholder='Password' name='password' value={password} onChange={inputOnChange}/>
