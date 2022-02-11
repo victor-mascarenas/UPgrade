70b04e4edbe2cdef5ff9ede2cdc8d37909d778e0
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-11 15:59:21 -0600

    Logged in status
    Checking if user is logged in, added 'waiting' page

---
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/auth/LobbyScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const LobbyScreen = () => {
+    return (
+        <div className='auth__main'>
+            <div className='auth__box-container text-center'>
+                <b>Checking your status</b>
+                <br/>
+                Please wait...
+            </div>
+        </div>
+    )
+}
--- React_Udemy_Fernando_Herrera/08-journal-app/src/routers/AppRouter.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/routers/AppRouter.js
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
 import { firebase } from '../firebase/firebase-config';
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
+import { LobbyScreen } from '../components/auth/LobbyScreen';
 
 export const AppRouter = () => {
     const dispatch = useDispatch();
+    const [checking, setChecking] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
             if (user?.uid) {
+                setIsLoggedIn(true);
                 dispatch(login(user.uid, user.displayName));
             } else {
-                
+                setIsLoggedIn(false);
             }
+            setChecking(false);
         });
-    }, []);
+    }, [dispatch, setChecking, setIsLoggedIn]);
 
+    if (checking) {
+        return <LobbyScreen/>;
+    }
     return (
         <BrowserRouter>
             <Routes>
