f1a6f00519643393114c20dbed37a628ad41d437
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-14 09:24:21 -0600

    Notes reducer
    Added notes reducer & loading NoteSection or NothingSelected conditionally in JournalScreen

--- React_Udemy_Fernando_Herrera/08-journal-app/src/components/journal/JournalScreen.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/components/journal/JournalScreen.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NoteSection } from '../notes/NoteSection'
 import { NothingSelected } from './NothingSelected'
 import { Sidebar } from './Sidebar'
 
 export const JournalScreen = () => {
+    const {active} = useSelector(state => state.notes);
+
     return (
         <div className='journal__main-content'>
             <Sidebar/>
             <main>
-                {/* <NothingSelected/> */}
-                <NoteSection/>
+                {
+                    (active) ?
+                        <NoteSection/> :
+                        <NothingSelected/>
+                }
             </main>
         </div>
     )
