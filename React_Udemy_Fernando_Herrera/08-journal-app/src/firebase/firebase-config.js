6497eac989d4f663550726a76762508468b28775
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-15 17:17:49 -0600

    Testing database config
    Done config for testing database

--- React_Udemy_Fernando_Herrera/08-journal-app/src/firebase/firebase-config.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/firebase/firebase-config.js
@@ -10,8 +10,20 @@ const firebaseConfig = {
     messagingSenderId: "969877029361",
     appId: "1:969877029361:web:deb00cdc6f7ecced42c450"
 };
+const firebaseTestingConfig = {
+    apiKey: "AIzaSyByHsrOsn3dbUnOYalCr0i7M-RBTVBereU",
+    authDomain: "react-fernando-herrera-demo.firebaseapp.com",
+    projectId: "react-fernando-herrera-demo",
+    storageBucket: "react-fernando-herrera-demo.appspot.com",
+    messagingSenderId: "298756334555",
+    appId: "1:298756334555:web:9b671b86be24a8ec243dfe"
+};
 
-firebase.initializeApp(firebaseConfig);
+if (process.env.NODE_ENV === 'test') {
+    firebase.initializeApp(firebaseTestingConfig);
+} else {
+    firebase.initializeApp(firebaseConfig);
+}
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
