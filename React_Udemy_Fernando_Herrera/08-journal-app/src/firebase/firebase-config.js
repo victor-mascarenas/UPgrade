5f3d4c3ddf51170110684c46d095e5d92932f0c4
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-16 15:50:41 -0600

    Environment variables
    Added environment variables for Firebase configuration

--- React_Udemy_Fernando_Herrera/08-journal-app/src/firebase/firebase-config.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/firebase/firebase-config.js
@@ -3,27 +3,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBTmXkHgRSXUMJlK5oiz14LbjHS2i5XHyU",
-    authDomain: "react-fernando-herrera-e248c.firebaseapp.com",
-    projectId: "react-fernando-herrera-e248c",
-    storageBucket: "react-fernando-herrera-e248c.appspot.com",
-    messagingSenderId: "969877029361",
-    appId: "1:969877029361:web:deb00cdc6f7ecced42c450"
-};
-const firebaseTestingConfig = {
-    apiKey: "AIzaSyByHsrOsn3dbUnOYalCr0i7M-RBTVBereU",
-    authDomain: "react-fernando-herrera-demo.firebaseapp.com",
-    projectId: "react-fernando-herrera-demo",
-    storageBucket: "react-fernando-herrera-demo.appspot.com",
-    messagingSenderId: "298756334555",
-    appId: "1:298756334555:web:9b671b86be24a8ec243dfe"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
-if (process.env.NODE_ENV === 'test') {
-    firebase.initializeApp(firebaseTestingConfig);
-} else {
-    firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
