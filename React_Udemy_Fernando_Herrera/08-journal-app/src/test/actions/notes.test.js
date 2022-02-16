04e66f3c5bfc8fddb43f6b8c77465e2007442b81
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-16 13:05:30 -0600

    Notes tests
    Notes actions (async, firestore) tests done

--- React_Udemy_Fernando_Herrera/08-journal-app/src/test/actions/notes.test.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/test/actions/notes.test.js
@@ -1,6 +1,7 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startNewNote } from '../../actions/notes';
+import { types } from '../../reducers/types/types';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -12,6 +13,21 @@ const store = mockStore({
 
 describe('Pruebas en acciones de notes', () => {
     test('Debe crear una nueva nota startNewDate', async () => {
+        const payload = {
+            id: expect.any(String),
+            title: '',
+            body: '',
+            date: expect.any(Number)
+        };
         await store.dispatch(startNewNote());
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: types.NOTES_ADD_NEW,
+            payload
+        });
+        expect(actions[1]).toEqual({
+            type: types.NOTES_ACTIVE,
+            payload
+        });
     });
 });
