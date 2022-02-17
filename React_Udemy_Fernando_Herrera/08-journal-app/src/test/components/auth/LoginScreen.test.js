5740021ce1d90072089e1fd4e1e79dd979332253
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-17 14:41:29 -0600

    Login Screen tests
    Tests done for LoginScreen component

--- React_Udemy_Fernando_Herrera/08-journal-app/src/test/components/auth/LoginScreen.test.js
+++ React_Udemy_Fernando_Herrera/08-journal-app/src/test/components/auth/LoginScreen.test.js
@@ -5,6 +5,18 @@ import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { MemoryRouter } from 'react-router-dom';
+import { startGoogleLogin, startLogin } from '../../../actions/auth';
+
+jest.mock('../../../actions/auth', () => ({
+    startGoogleLogin: jest.fn(),
+    startLogin: jest.fn()
+}));
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useDispatch: jest.fn(() => {
+        return () => {}
+    })
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -31,9 +43,20 @@ const wrapper = mount(
 describe('Pruebas en LoginScreen', () => {
     beforeEach(() => {
         store = mockStore(initState);
+        jest.clearAllMocks();
     });
     
     test('Debe de mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+    test('Debe de disparar la accion de startGoogleLogin', () => {
+        wrapper.find('.google-btn').simulate('click');
+        expect(startGoogleLogin).toHaveBeenCalledTimes(1);
+    });
+    test('Debe de disparar el startLogin con los respectivos argumentos', () => {
+        wrapper.find('form').simulate('submit', {
+            preventDefault: jest.fn()
+        });
+        expect(startLogin).toHaveBeenCalledWith('nando@gmail.com', '123456');
     });
 });
