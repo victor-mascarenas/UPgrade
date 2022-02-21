daa199436383b57b32e79eb9352db5190effcabc
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-21 14:59:18 -0600

    Express
    Express configuration done

--- React_Udemy_Fernando_Herrera/10-calendar-backend/index.js
+++ React_Udemy_Fernando_Herrera/10-calendar-backend/index.js
@@ -1,1 +1,15 @@
-console.log('hola mundo');
+const express = require('express');
+
+const port = 3001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`);
+});
