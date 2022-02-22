20e605bd62059bfdbc9cceb8edbf4660945ebd1c
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-22 09:35:43 -0600

    Validator middleware
    Practice with custom middlewares creating a validator function

--- React_Udemy_Fernando_Herrera/10-calendar-backend/controllers/auth.js
+++ React_Udemy_Fernando_Herrera/10-calendar-backend/controllers/auth.js
@@ -1,40 +1,24 @@
 const {response} = require("express");
-const {validationResult} = require('express-validator');
 
 const createUser = (req, res = response) => {
     let resp;
     const {name, email, password} = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        resp = res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    } else {
-        resp = res.status(201).json({
-            ok: true,
-            msg: 'register',
-            name,
-            email,
-            password
-        });
-    }
+    resp = res.status(201).json({
+        ok: true,
+        msg: 'register',
+        name,
+        email,
+        password
+    });
     return resp;
 };
 const userLogin = (req, res = response) => {
     let resp;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        resp = res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    } else {
-        resp = res.json({
-            ok: true,
-            msg: 'login'
-        });
-    }
+    resp = res.json({
+        ok: true,
+        msg: 'login'
+    });
+    return resp;
 };
 const revalidateToken = (req, res = response) => {
     res.json({
--- React_Udemy_Fernando_Herrera/10-calendar-backend/routes/auth.js
+++ React_Udemy_Fernando_Herrera/10-calendar-backend/routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const {createUser, userLogin, revalidateToken} = require('../controllers/auth');
 const {check} = require('express-validator');
+const validateFields = require('../middlewares/field-validator');
 
 const router = Router();
 
@@ -12,13 +13,15 @@ router.post('/new', [
     check('email', 'El email no es valido').isEmail(),
     check('password', 'La contraseña debe contener al menos 6 caracteres').isLength({
         min: 6
-    })
+    }),
+    validateFields
 ], createUser);
 router.post('/', [
     check('email', 'El email no es valido').isEmail(),
     check('password', 'La contraseña debe contener al menos 6 caracteres').isLength({
         min: 6
-    })
+    }),
+    validateFields
 ], userLogin);
 router.get('/renew', revalidateToken);
 
