35182bf1cd83fb4841b54618095a907eed01d199
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-22 14:39:44 -0600

    CORS
    Done CORS config

--- React_Udemy_Fernando_Herrera/10-calendar-backend/index.js
+++ React_Udemy_Fernando_Herrera/10-calendar-backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const dbConnection = require('./database/config.js');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 
 dbConnection();
 
+app.use(cors());
 app.use(express.static('public'));
-
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.js'));
