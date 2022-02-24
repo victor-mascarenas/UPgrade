9a5531eb48392411e500b18c2162d7fb27ec9bb4
victor-mascarenas <victor-javier.garcia@atos.net>
2022-02-24 15:59:13 -0600

    fetchWithToken helper test
    Test done for fetchWithToken helper

--- React_Udemy_Fernando_Herrera/09-calendar-app/src/test/helpers/fetch.test.js
+++ React_Udemy_Fernando_Herrera/09-calendar-app/src/test/helpers/fetch.test.js
@@ -1,7 +1,9 @@
-import { fetchWithoutToken } from "../../helpers/fetch";
+import { fetchWithoutToken, fetchWithToken } from "../../helpers/fetch";
 
 describe('Pruebas en fetch', () => {
-    test('fetchWithoutToken ebe de funcionar', async () => {
+    let token;
+
+    test('fetchWithoutToken debe de funcionar', async () => {
         const resp = await fetchWithoutToken('auth', {
             email: 'mario@gmail.com',
             password: '123456'
@@ -9,5 +11,13 @@ describe('Pruebas en fetch', () => {
         expect(resp).toBeInstanceOf(Response);
         const body = await resp.json();
         expect(body.ok).toBeTruthy();
+        token = body.token;
+    });
+    test('fetchWithToken debe de funcionar', async () => {
+        localStorage.setItem('token', token);
+        const resp = await fetchWithToken('events/6217d8bd334d31dcb56b7517', {}, 'DELETE');
+        expect(resp).toBeInstanceOf(Response);
+        const body = await resp.json();
+        expect(body.msg).toBe('El evento solicitado no existe');
     });
 });
