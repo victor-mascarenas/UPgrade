f2741665d2fef513a71de0f7999f21b6e0e2a661
victor-mascarenas <victor-javier.garcia@atos.net>
2022-03-03 15:14:40 -0600

    DeleteEventFab tests
    Done tests for DeleteEventFab component

--- React_Udemy_Fernando_Herrera/09-calendar-app/src/test/components/DeleteEventFab.test.js
+++ React_Udemy_Fernando_Herrera/09-calendar-app/src/test/components/DeleteEventFab.test.js
@@ -4,21 +4,30 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import { DeleteEventFab } from '../../components/ui/DeleteEventFab';
+import { eventStartDelete } from '../../actions/events';
+
+jest.mock('../../actions/events', () => ({
+    eventStartDelete: jest.fn()
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const initState = {};
 const store = mockStore(initState);
+store.dispatch = jest.fn();
+
 const wrapper = mount(
     <Provider store={store}>
         <DeleteEventFab/>
     </Provider>
 );
 
-store.dispatch = jest.fn();
-
 describe('Pruebas en DeleteEventFab component', () => {
     test('Debe mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
+    });
+    test('Debe de llamar el eventStartDelete al hacer click', () => {
+        wrapper.find('button').simulate('click');
+        expect(eventStartDelete).toHaveBeenCalled();
     });
 });
