924c74866e51b8b8a0aa3d7fe125814d75f6fc4f
victor-mascarenas <victor-javier.garcia@atos.net>
2022-03-03 15:30:16 -0600

    AppRouter tests done
    Done tests for AppRouter component

--- React_Udemy_Fernando_Herrera/09-calendar-app/src/test/router/AppRouter.test.js
+++ React_Udemy_Fernando_Herrera/09-calendar-app/src/test/router/AppRouter.test.js
@@ -4,15 +4,9 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import { AppRouter } from '../../router/AppRouter';
-// import { eventStartDelete } from '../../actions/events';
-
-/* jest.mock('../../actions/events', () => ({
-    eventStartDelete: jest.fn()
-})); */
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
-// store.dispatch = jest.fn();
 
 describe('Pruebas en AppRouter', () => {
     test('Debe de mostrar el "espere"', () => {
@@ -28,5 +22,44 @@ describe('Pruebas en AppRouter', () => {
             </Provider>
         );
         expect(wrapper).toMatchSnapshot();
+    });
+    test('Debe demostrar la ruta publica', () => {
+        const initState = {
+            auth: {
+                checking: false,
+                uid: null
+            }
+        };
+        const store = mockStore(initState);
+        const wrapper = mount(
+            <Provider store={store}>
+                <AppRouter/>
+            </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.login-container').exists()).toBeTruthy();
+    });
+    test('Debe demostrar la ruta privada', () => {
+        const initState = {
+            auth: {
+                checking: false,
+                uid: '123',
+                name: 'Victor'
+            },
+            ui: {
+                modalOpen: false
+            },
+            calendar: {
+                events: []
+            }
+        };
+        const store = mockStore(initState);
+        const wrapper = mount(
+            <Provider store={store}>
+                <AppRouter/>
+            </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.calendar-screen').exists()).toBeTruthy();
     });
 });
